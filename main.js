4471932494c01ad5aaac86c0ba5b5ae323762143
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-04-24 23:36:56 -0400

    CSS & HTML for Snake

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 /*----- constants -----*/
 
+const TARGET = {
+    color: 'red'
+};
+
+
+
 /*----- state variables -----*/
+
+let board; //40 x 40 array
+let currentScore; // p will be for player results
+
+
 /*----- cached elements  -----*/
+
+const snakeBoard = document.querySelector('#snake-board'); //capture the 
+
+
+
 /*----- event listeners -----*/
+
 /*----- functions -----*/
