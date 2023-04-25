95b4c7e05d22d1d9be477f908933222534bdf70c
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-04-25 13:47:17 -0400

    Update render function, add renderBoard JS function

--- main.js
+++ main.js
@@ -7,24 +7,26 @@
 
 let board; //40 x 40 array
 let currentScore; // p will be for player results
+let elementLocation; //'s' for snake and 't' for target
+let direction; // 's' for snake
 
 
 /*----- cached elements  -----*/
 
-const snakeBoardEl = document.querySelector('#snake-board'); //Capture the board
+const snakeBoardEl = document.querySelector('#snake-board'); // Capture the board
 
 
 //Add snake to snakeBoard
 const snakeEl = document.querySelector('.snake'); 
-snakeEl.style.gridRowStart = 5;
-snakeEl.style.gridColumnStart = 5; 
-snakeBoardEl.appendChild(snakeEl);
+// snakeEl.style.gridRowStart = 5;
+// snakeEl.style.gridColumnStart = 5; 
+// snakeBoardEl.appendChild(snakeEl);
 
 //Add target Object to snakeBoard
 const targetEl = document.querySelector('.target');
-targetEl.style.gridRowStart = 7;
-targetEl.style.gridColumnStart = 8; 
-snakeBoardEl.appendChild(targetEl);
+// targetEl.style.gridRowStart = 7;
+// targetEl.style.gridColumnStart = 8; 
+// snakeBoardEl.appendChild(targetEl);
 
 
 /*----- event listeners -----*/
@@ -41,8 +43,14 @@ snakeBoardEl.appendChild(targetEl);
          this.direction = direction; //direction of snake
      }
 
-     
+     move () {
+        //Updating the position of the snake in grid
+        this.position.x += this.direction.x; //this statement = this.position.x = this.position.x + this.direction.x
+        this.position.y += this.direction.y;
 
+        //Update the position in CSS leveraging DOM
+        
+     }
  }
 
 
@@ -50,14 +58,29 @@ snakeBoardEl.appendChild(targetEl);
 /*----- functions -----*/
  init();
 
- function init () {
+
+ function init() {
     //Instantiating snake class
-    snakeObject1 = new Snake1(body, 5, 5, direction);
+    //snakeObject1 = new Snake1(body, 5, 5, direction);
+    render();
+ }
 
+ 
+ function render() {
     //Render's message to Snake Board
-    //function renderMessage();
-
-
+    //renderMessage();
+    //Render board
+    renderBoard();
  }
 
 
+ function renderBoard() {
+    snakeEl.style.gridRowStart = 5;
+    snakeEl.style.gridColumnStart = 5; 
+    snakeBoardEl.appendChild(snakeEl);
+
+    targetEl.style.gridRowStart = 7;
+    targetEl.style.gridColumnStart = 8; 
+    snakeBoardEl.appendChild(targetEl);
+
+ }
