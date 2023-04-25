bd0b9143282959ce66ae40fd097c15456861b748
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-04-25 18:07:40 -0400

    Added snake grow method

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /*----- constants -----*/
 
 const COLOR = {
-    '0': 'lightpink',
-    '1': 'B77CE9',
-    '2': ''
+    '0': 'lightpink', //grid color
+    '1': snakeEl, //snake element
+    '2': targetEl, //target element
 };
 
 
@@ -26,17 +26,18 @@ const targetEl = document.querySelector('.target');
 
 /*----- event listeners -----*/
 
+document.addEventListener('')
+
 
 
 
 
 /*----- classes -----*/
  class Snake1 {
-     constructor(x, y, direction) {
+     constructor(body, length, x, y, direction) {
 //         //this.snakeEl = snakeEl; //new snakeEl
-         this.body = [
-            {x:x, y:y} //Square element
-         ];
+         this.body = [ {x:x, y:y}]; //Square element
+         this.length = 1; //length of snake
          this.position = {x: 5, y: 5}; //position of snake
          this.direction = right; //direction of snake
      }
@@ -57,7 +58,9 @@ const targetEl = document.querySelector('.target');
      }
 
      changeDirection () {
-        function change(direction) {
+        //if the player hits the up, right, down, left arrow key the
+        //the snake should move
+        function snakeDirection(direction) {
             //Arrow Up
             if(this.direction !== "down") {
                 this.direction = "up";
@@ -68,16 +71,30 @@ const targetEl = document.querySelector('.target');
             } else if(this.direction !== "up") {
                 this.direction = "down";
             //ArrowLeft
-            } else (this.direction !== "right") {
+            } else if (this.direction !== "right") {
                 this.direction = "left";
             }
         }
      }
 
      grow (){
+        //If the snake bumps into an element on the grid with a value of 2 
+        //then it should grow in size by +1 square
+        const snakeBody = this.body[this.body.length -1];
+        
+        function snakeGrow (direction) {
+            //if snake is moving up
+            if(this.direction === "up"){
+                this.body.push({x: snakeBody.x, y:snakeBody.y - 1})
+            } else if (this.direction === "right") {
+                this.body.push({x: snakeBody.x + 1, y: snakeBody.y})
+            } else if (this.direction === "down") {
+                this.body.push({x: snakeBody.x + 1, y: snakeBody.y + 1})
+            } else if (this.direction === "left") {
+                this.body.push({x: snakeBody.x - 1, y: snakeBody.y})
+        }
 
      }
-
 
     }
  
@@ -88,7 +105,7 @@ const targetEl = document.querySelector('.target');
 
  function init() {
 
-    snakeBoard = [
+const snakeBoard = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -103,7 +120,7 @@ const targetEl = document.querySelector('.target');
 
 
     //Instantiating snake class
-    //snakeObject1 = new Snake1(body, 0, 0, direction);
+    //snakeObject1 = new Snake1(body, 1, 0, 0, right);
     render();
  }
 
@@ -114,14 +131,13 @@ const targetEl = document.querySelector('.target');
 
 
  function renderBoard() {
-
+    //Adding Snake to Board
     const addSnakeEl = document.querySelector('.snake');
     snakeBoardEl.appendChild(snakeEl);
 
- 
+    //Adding Target to Board
     const addTargetEl = document.querySelector('.target');
     snakeBoardEl.appendChild(targetEl);
-
 
     
  }
