23599b4177c1327dc01b34ef81d965120121f414
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-04-25 22:43:01 -0400

    Updated Directions object, Updated variable in class

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 /*----- constants -----*/
 
-const COLOR = {
-    '0': 'lightpink', //grid color
-    '1': document.querySelector('.snake'), //snake element
-    '2': document.querySelector('.target'), //target element
-};
-
 const DIRECTIONS = {
-        up: "up",
-        down: "down",
-        left: "left",
-        right: "right",
+    up: { x: 0, y: -1 },
+    down: { x: 0, y: 1 },
+    left: { x: -1, y: 0 },
+    right: { x: 1, y: 0 },
 };
 
-const x = 5; //starting x position
-const y = 5; //starting y position
+// const x = 5; //starting x position
+// const y = 5; //starting y position
 
 /*----- state variables -----*/
 
@@ -45,53 +39,48 @@ const targetEl = document.querySelector('.target');
          this.body = [{x:x, y:y}];
          this.length = 1; //length of snake
          this.position = {x:x, y:y}; //position of snake
-         this.direction = 'right'; //direction of snake
+         this.direction = DIRECTION[direction] //direction of snake
 
          document.addEventListener('keydown', function(event) {
             this.changeDirection(event.code);
           });
+
+          //Call move method
      }
      
 
      move () {
-        //Updating the position of the snake in grid
-        this.position.x += this.direction.x; //this statement = this.position.x = this.position.x + this.direction.x
+        this.position.x += this.direction.x;
         this.position.y += this.direction.y;
 
-        //Update the position in CSS leveraging CSS & DOM
-        this.style.gridColumnStart = this.position.x;
-        this.style.gridRowStart = this.position.y;
-
-        //interval for how fast the snake will move
-        setInterval(function(){
-           this.move();
-        }, 1000);
-     }
+        this.snakeEl.style.gridColumnStart = this.position.x + 1;
+        this.snakeEl.style.gridRowStart = this.position.y + 1;
+    }
 
      changeDirection (code) {
         //if the player hits the up, right, down, left arrow key the
         //the snake should move
         switch (code) {
             case "ArrowUp":
-                if(this.direction !== "down") {
-                    this.direction = "up";
+                if(this.direction !== DIRECTIONS.down) {
+                    this.direction = DIRECTIONS.up;
                 }
                 break;
 
             case "ArrowRight":
-                if (this.direction !== "left") {
-                    this.direction = "right";
+                if (this.direction !== DIRECTIONS.left) {
+                    this.direction = DIRECTIONS.right;
                 }
                 break;
             
             case "ArrowDown":
-                if(this.direction !== "up") {
-                    this.direction = "down";
+                if(this.direction !== DIRECTIONS.up) {
+                    this.direction = DIRECTIONS.down;
                 } 
             
             case "ArrowLeft":
-                if (this.direction !== "right") {
-                        this.direction = "left";
+                if (this.direction !== DIRECTIONS.right) {
+                        this.direction = DIRECTIONS.left;
                     }
                 break;
         }
@@ -103,25 +92,25 @@ const targetEl = document.querySelector('.target');
         const snakeBody = this.body[this.body.length -1];
         
         switch (this.direction) {
-            case 'up':
+            case DIRECTIONS.up:
                 if(this.direction === "up"){
                     this.body.push({x: snakeBody.x, y: snakeBody.y - 1});
                 } 
                 break;
             
-            case 'right':
+            case DIRECTIONS.right:
                 if (this.direction === "right") {
                     this.body.push({x: snakeBody.x + 1, y: snakeBody.y});
                 }
                 break;
             
-            case 'down':
+            case DIRECTIONS.down:
                 if (this.direction === "down") {
                     this.body.push({x: snakeBody.x + 1, y: snakeBody.y + 1});
                 }
                 break;
             
-            case 'left':
+            case DIRECTIONS.left:
                 if (this.direction === "left") {
                 this.body.push({x: snakeBody.x - 1, y: snakeBody.y});
                 }
@@ -186,7 +175,8 @@ function init() {
   
  }
 
- function gameLogic {
+
+ function gameLogic() {
 
 
 
